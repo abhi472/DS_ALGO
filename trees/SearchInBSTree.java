d5f791177c90c6f131d0241bbaf86eba01e28732
Abhishek Tiwari <abhitiwari472@gmail.com>
2023-11-28 03:06:30 +0530

    added iterative solution for search

--- trees/SearchInBSTree.java
+++ trees/SearchInBSTree.java
@@ -5,20 +5,32 @@ public class SearchInBSTree {
     public static void main(String[] args) {
        
         TreeNode node = PreparedTree.setBSTreeNode();
-        System.out.println(contains(node, 4));
+        System.out.println(containsIterative(node, 4));
     }
-       
-
     public static boolean contains(TreeNode node, int x) {
 
         if (node == null)
             return false;
+        
         if(node.data == x)
             return true;
         if(node.data > x)
             return contains(node.left, x);
         else return contains(node.right, x);
 
+    }
+
+    public static boolean containsIterative(TreeNode node, int x) {
+
+        while (node != null) {
+            if(node.data == x)
+                return true;
+            else if(node.data > x)
+                node = node.left;
+            else node = node.right;
+        }
+
+        return false;
     }
 
 }
